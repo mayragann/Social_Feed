10922657c11738726559453d448f0171b7ba17dd
mayragann <mayragann@gmail.com>
2022-04-20 15:12:22 -0400

    Added CreatePosts to Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
+import CreatePost from './Components/CreatePost/CreatePost';
 import DisplayPosts from './Components/DisplayPosts/DisplayPosts';
 
 function App() {
   const [posts, setPosts] = useState([{name:'Richard Phillips', post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}, {name:'Stacey Williams', post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}])
-
+  
+  function addNewEntry(entry){
+    let tempPosts = [ ...posts, entry];
+    setPosts(tempPosts);
+  }
 
   return (
     <div>
       <h3>Hello World</h3>
       <div>
+        <CreatePost addNewEntryProperty={addNewEntry}/>
         <DisplayPosts parentPosts={posts} />
       </div>
     </div>
--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -1,36 +1,36 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
 
-// const CreatePost = (prop) => {
+const CreatePost = (props) => {
 
-//     const [name, setName] = useState('');
-//     const [post, setPost] = useState('');
+    const [name, setName] = useState('');
+    const [post, setPost] = useState('');
 
-//     function handleSubmit(event){
-//         event.PreventDefault();
-//         let newEntry ={
-//                 name: name,
-//                 post: post
+    function handleSubmit(event){
+        event.PreventDefault();
+        let newEntry ={
+                name: name,
+                post: post
 
-//         }
-//         console.log(NewEntry)
-//         PaymentResponse.addNewEntryProperty(newEntry)
-//     }
-//     return(
-//         <form onSubmit={handleSubmit}>
-//             <div>
-//                 <label>Name</label>
-//                 <input type='text' value={name} onChange={(event) => setName(event.target.value)}/>
-//             </div>
-//             <div>
-//                 <label>Post</label>
-//                 <input type='text' value={name} onChange={(event) => setPost(event.target.value)}/>
-//             </div>
-//             <button type='submit'>Create</button>
-//         </form>
+        }
+        console.log(newEntry)
+        props.addNewEntryProperty(newEntry)
+    }
+    return(
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Name</label>
+                <input type='text' value={name} onChange={(event) => setName(event.target.value)}/>
+            </div>
+            <div>
+                <label>Post</label>
+                <input type='text' value={post} onChange={(event) => setPost(event.target.value)}/>
+            </div>
+            <button type='submit'>Create</button>
+        </form>
 
-//     );
+    );
 
-// }
+}
 
-// export default CreatePost;
+export default CreatePost;
